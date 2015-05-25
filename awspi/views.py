b8fc8e7295e6827c786876a166ca5256e46edbae
Wang Fei <wangfei@cyou-inc.com>
2015-05-25 19:44:17 +0800

    add s3pi list ojects function

--- awspi/views.py
+++ awspi/views.py
@@ -7,21 +7,20 @@ import MySQLdb
 def HoursLater(request,offset):
     try:
         offset = int(offset)
-        
+
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
     return render_to_response('hours_later.html',{'offset':offset,'then':dt})
-    
+
 def current_time(request):
         now = datetime.datetime.now()
         return render_to_response('current_time.html',{'current_time':now})
 
 def show_users(request):
-    db = MySQLdb.connect(user='root',db='mysql',passwd='Ch@ngy0u.com',host='127.0.0.1')
+    db = MySQLdb.connect()
     cursor = db.cursor()
     cursor.execute('SELECT user,host FROM user')
     users = [row for row in cursor.fetchall()]
     db.close()
     return render_to_response('user_list.html',{'users':users})
-
--- s3pi/views.py
+++ s3pi/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
+import boto
+
+def get_object(request):
+    bucket_name = 'cypay-filesharing'
+    conn = boto.connect_s3()
+    b = conn.lookup(bucket_name)
+    ob = b.list()
+    object_list = []
+    for i in ob:
+        object_list.append("s3://" + bucket_name + "/" + i.name)
+    return render_to_response('listbucket.html',{"object_list":object_list})
